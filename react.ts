07c329d1b0fd846de9f8910e8d58eee97e6ac4ec
RobPruzan <rapruzan@buffalo.edu>
2024-08-09 20:56:16 -0700

    before weird output

--- react.ts
+++ react.ts
@@ -1,42 +1,4 @@
-/**
- *
- *
- * End state im too tired
- *
- *
- * i do want the tree lazily evaluated how its currently happening so we can control the renderings more
- *
- *
- * I want a component to return an "element" object. I don't care what the internal representation is
- *
- * the element object should take the tag | component, props and children
- *
- * we can than enhance that into a fiber which includes state and effects
- *
- *
- * and the actual tree that holds all that is just an abstraction on top of the fiber
- *
- *
- * so something like:
- *
- *
- * returns -> ReactElement
- *
- *
- * ReactElement immediately lifted to -> ReactFiber
- *
- *
- * ReactFiber rendered to generate -> ReactInternalTree
- *
- *
- * we are currently pretty close to that, we just need the ReactElement -> ReactFiber lifting intermediary step
- *
- *
- *
- * late night thought, have Internal types and External types, we immediately lift external types to internal types to make it easier to process
- */
-
-type AnyProps = Record<string, unknown>;
+type AnyProps = Record<string, unknown> | null;
 type run = <T>(f: () => T) => T;
 
 type ReactComponentFunction<T extends AnyProps> = (
@@ -131,54 +93,6 @@ const makeTagNode = (
   },
 });
 
-// const renderComponent = ({
-//   component,
-//   parentNode,
-// }: {
-//   parentNode: ReactTreeNode;
-//   component: ReactComponentInternalMetadata["component"];
-//   }): ReactTreeNode => {
-//   if (!currentTreeRef.tree) {
-//     throw new Error("Cannot render component outside of react tree")
-//   }
-//   switch (component.kind) {
-//     case "tag": {
-//       // its a leaf node
-//       const newNode = makeTagNode(component.tagName);
-//       parentNode.childNodes.push(newNode);
-//       return newNode;
-//     }
-
-//     case "function": {
-
-//       const returnedInternalMetadata = component.function(
-//         parentNode.metadata.props
-//       );
-
-//       const newNode: ReactTreeNode = {
-//         id: crypto.randomUUID(),
-//         metadata: returnedInternalMetadata,
-//         childNodes: [],
-//       };
-//       returnedInternalMetadata.children.forEach((childComponent) => {
-//         const childNode = renderComponent({
-//           component: childComponent,
-//           parentNode: newNode,
-//         });
-//         // avoid appending the leaf twice
-//         if (childNode.metadata.component.kind === "function") {
-//           newNode.childNodes.push(childNode);
-//         }
-//       });
-
-//       parentNode.childNodes.push(newNode);
-
-//       return newNode;
-//     }
-//   }
-//   component satisfies never;
-// };
-
 const renderComponent = ({
   internalMetadata,
   parentNode,
@@ -189,26 +103,19 @@ const renderComponent = ({
   if (!currentTreeRef.tree) {
     throw new Error("Cannot render component outside of react tree");
   }
+  currentTreeRef.tree.currentlyRendering = internalMetadata;
 
   const newNode: ReactTreeNode = {
     id: crypto.randomUUID(),
     metadata: internalMetadata,
     childNodes: [],
   };
-  // if (parentNode)
   parentNode.childNodes.push(newNode);
   switch (internalMetadata.component.kind) {
     case "tag": {
-      // const tagNode = makeTagNode(
-      //   internalMetadata.component.tagName,
-      //   internalMetadata.id
-      // );
-      // parentNode.childNodes.push(tagNode);
       break;
-      // return tagNode;
     }
     case "function": {
-      currentTreeRef.tree.currentlyRendering = internalMetadata;
       const childMetadata = internalMetadata.component.function(
         internalMetadata.props
       );
@@ -285,7 +192,7 @@ const useState = <T>(initialValue: T) => {
     (value: T) => {
       hookMetadata.value = value;
     },
-  ];
+  ] as const;
 };
 
 const Bar = () => {
@@ -295,21 +202,26 @@ const Foo = () => {
   const foo = createElement(
     "div",
     {},
-    createElement("div", {}),
-    createElement(Bar, {})
+    createElement("div", null),
+    createElement(Bar, null)
   );
   return foo;
 };
 
 const Component = () => {
-  // const [x, setX] = useState(2);
+  const [x, setX] = useState(2);
   return createElement(
     "div",
     {},
-    createElement(Foo, {}),
-    createElement("div", {}),
-    createElement("span", {})
+    createElement(Foo, null),
+    createElement("button", {
+      innerText: x,
+      onClick: () => {
+        setX(x + 1);
+      },
+    }),
+    createElement("span", null)
   );
 };
 
-buildReactTree(Component());
+buildReactTree(createElement(Component, null));
